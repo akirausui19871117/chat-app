be56fe015a66a9463f08f91a0c11f1e32a4dbdd6
akirausui19871117 <akirausui19871117@users.noreply.github.com>
2021-02-05 00:57:47 +0900

    各モデルのテスト

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_160444) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
